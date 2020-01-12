be037bdb90ceafaa287a7688af0083d4da3728ab
Brian Allen <brianallen2401@gmail.com>
2020-01-12 16:22:17 -0700

    added array of words and wins variables to JS doc

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+var wins = 0;
+var wordSelectionArray = [
+    "adventure", "bandit", "battleaxe", "battlefield", "beholder", "bugbear", "castle", "catapult", "cleric", "dragon", "dungeon", 
+    "fairy", "fireball", "giant", "goblin", "knight", "magic", "monster", "necromancer", "owlbear", "paladin", "potion", "quarterstaff", 
+    "sorcerer", "spells", "sword", "vampire", "warlock", "wizard", "zombie"]
+var game = {
+
+}
