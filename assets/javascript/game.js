2449a159a100028f7e09dc8e89fb59a6fbdb5f14
Brian Allen <brianallen2401@gmail.com>
2020-01-12 16:46:48 -0700

    added comments to first sections of code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,9 @@
+// start the game with a score of zero
 var wins = 0;
+// words to guess will be selected from here
 var wordSelectionArray = [
     "adventure", "bandit", "battleaxe", "battlefield", "beholder", "bugbear", "castle", "catapult", "cleric", "dragon", "dungeon", 
     "fairy", "fireball", "giant", "goblin", "knight", "magic", "monster", "necromancer", "owlbear", "paladin", "potion", "quarterstaff", 
     "sorcerer", "spells", "sword", "vampire", "warlock", "wizard", "zombie"]
-var 
-
-
-
-
-
-
-
-
-
-    var game = {
-
-}
+//this function will select the word for us to guess.
+function getWord() {}
