081137d936bfc82d1d8dffbb84788e157d9a5d7e
Brian Allen <brianallen2401@gmail.com>
2020-01-12 16:35:40 -0700

    added game object and spacing to js. added container to content divs in html.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,16 @@ var wordSelectionArray = [
     "adventure", "bandit", "battleaxe", "battlefield", "beholder", "bugbear", "castle", "catapult", "cleric", "dragon", "dungeon", 
     "fairy", "fireball", "giant", "goblin", "knight", "magic", "monster", "necromancer", "owlbear", "paladin", "potion", "quarterstaff", 
     "sorcerer", "spells", "sword", "vampire", "warlock", "wizard", "zombie"]
-var game = {
+var 
+
+
+
+
+
+
+
+
+
+    var game = {
 
 }
