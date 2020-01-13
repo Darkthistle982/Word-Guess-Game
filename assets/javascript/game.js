5db295bd0965987063716b41b7bb8f80e4754abc
Brian Allen <brianallen2401@gmail.com>
2020-01-12 19:30:49 -0700

    wrote and tested a function to take the random word, and split it into letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,21 +7,35 @@ var wordSelectionArray = [
     "sorcerer", "spells", "sword", "vampire", "warlock", "wizard", "zombie"]
 //variable to hold number of guesses remaining
 var guessesRemaining = 12;
+//variable to hold the randomly selected word
 var selectedWord;
+//variable to hold the split out word as an array
+var splitWord = [];
+
+
 //function to choose a word from the array
 function getWord() {
     wordInt = Math.floor((Math.random)() * wordSelectionArray.length);
     selectedWord = wordSelectionArray[wordInt];
     console.log(selectedWord);
 }
+//funciton to split the random word into an array of letters
+ function wordSplit() {
+ var splitUpWords = selectedWord.split("");
+ console.log(splitUpWords);
 
-
-
+}
+//function to display the guesses remaining
+function guessesLeft() {
+    document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
+}
 
 //function to display the current score
 function totalWins() {
     document.getElementById("wins").innerHTML = wins;
 }
-
-totalWins();
 getWord();
+guessesLeft();
+totalWins();
+wordSplit();
+// console.log();
