c7710c51b60589903460d9935e7330f8722a4c89
Brian Allen <brianallen2401@gmail.com>
2020-01-13 12:41:19 -0700

    deleted non-working function. added a blanks variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,19 @@
 var wins = 0;
 // words to guess will be selected from here
 var wordSelectionArray = [
-    "adventure", "bandit", "battleaxe", "battlefield", "beholder", "bugbear", "castle", "catapult", "cleric", "dragon", "dungeon", 
-    "fairy", "fireball", "giant", "goblin", "knight", "magic", "monster", "necromancer", "owlbear", "paladin", "potion", "quarterstaff", 
+    "adventure", "bandit", "battleaxe", "battlefield", "beholder", "bugbear", "castle", "catapult", "cleric", "dragon", "dungeon",
+    "fairy", "fireball", "giant", "goblin", "knight", "magic", "monster", "necromancer", "owlbear", "paladin", "potion", "quarterstaff",
     "sorcerer", "spells", "sword", "vampire", "warlock", "wizard", "zombie"]
 //variable to hold number of guesses remaining
 var guessesRemaining = 12;
 //variable to hold the randomly selected word
 var selectedWord;
+//variable to identify a letter
+var isLetter = ["abcdefghijklmnopqrstuvwxyz"];
+//variable to store number of blanks
+var blanks = 0;
+
+
 
 
 //function to choose a word from the array
@@ -16,13 +22,13 @@ function getWord() {
     wordInt = Math.floor((Math.random)() * wordSelectionArray.length);
     selectedWord = wordSelectionArray[wordInt];
 }
-    //funciton to split the random word into an array of letters
-    function wordSplit() {
-        var letters = selectedWord.split("");
-        console.log(letters);
-        
+//funciton to split the random word into an array of letters
+function wordSplit() {
+    var letters = selectedWord.split("");
+    console.log(letters);
+
 };
-    
+
 
 //function to display the guesses remaining
 function guessesLeft() {
