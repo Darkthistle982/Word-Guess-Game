9dbf0443bcb733e7f205b0ed2cf4885e305dd99f
Brian Allen <brianallen2401@gmail.com>
2020-01-20 17:40:25 -0700

    added return to correct guess function so guesses remaining is not decremented on a correct guess.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -56,6 +56,7 @@ function checkInput(letter) {
         for (var i = 0; i < blanks; i++) {
             if (selectedWord[i] == letter) {
                 blankAndCorrect[i] = letter;
+                return;
             }
         }
     
